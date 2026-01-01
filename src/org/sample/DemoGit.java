996f2a046d1b964b72c6beda57f7e2da8a20dc25
Afsal5868 <mahmoodafsalkather@gmail.com>
2026-01-01 04:42:02 -0800

    Project

---
+++ src/org/sample/DemoGit.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class DemoGit {
+	public static void main(String[] args) {
+		int a =2026;
+		System.out.println(a);
+		String s ="Afsal";
+		System.out.println(s);
+	}
+
+}
